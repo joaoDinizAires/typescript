a56b84deff9a84eef81eeb0f6d392a0361ac59e3
João Victor Diniz <joaodnz@proton.me>
2024-04-19 13:59:42 -0300

    Create exercicio.ts

---
+++ src/exercicio.ts
@@ -0,0 +1,8 @@
+function multiply(n:number,n2:number):number{
+    return n * n2;
+}
+function greet(name:string):string{
+    return `Olá ${name}`;
+}
+console.log(multiply(3,5));
+console.log(greet("Marcos"));
